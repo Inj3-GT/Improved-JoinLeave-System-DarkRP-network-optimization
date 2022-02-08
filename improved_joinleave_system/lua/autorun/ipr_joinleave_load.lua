482953d630d22353e3f3939a4bab83c8dbaff862
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2022-02-08 23:50:09 +0100

    1.0
    - Modification of the path/structure of the folders.
    - Some important changes.

---
+++ improved_joinleave_system/lua/autorun/ipr_joinleave_load.lua
@@ -0,0 +1,35 @@
+--- Script By Inj3 
+--- Script By Inj3 
+--- Script By Inj3 
+ImprovedJoinLeaveSys = ImprovedJoinLeaveSys or {}
+local Ipr_ConfigurationSys = file.Find("ipr_joinleave_sys/configuration/*", "LUA")
+local Ipr_ClientSys = file.Find("ipr_joinleave_sys/lua_file/ipr_joinleave_client/*", "LUA")
+
+if SERVER then
+     local Ipr_ServerSys = file.Find("ipr_joinleave_sys/lua_file/ipr_joinleave_server/*", "LUA")
+
+     for count, file in pairs(Ipr_ConfigurationSys) do
+          include("ipr_joinleave_sys/configuration/"..file)
+          AddCSLuaFile("ipr_joinleave_sys/configuration/"..file)
+     end
+
+     for count, file in pairs(Ipr_ServerSys) do
+          include("ipr_joinleave_sys/lua_file/ipr_joinleave_server/"..file)
+     end
+
+     for count, file in pairs(Ipr_ClientSys) do
+          AddCSLuaFile("ipr_joinleave_sys/lua_file/ipr_joinleave_client/"..file)
+     end
+end
+
+if CLIENT then
+     for count, file in pairs(Ipr_ConfigurationSys) do
+          include("ipr_joinleave_sys/configuration/"..file)
+     end
+
+     for count, file in pairs(Ipr_ClientSys) do
+          include("ipr_joinleave_sys/lua_file/ipr_joinleave_client/"..file)
+     end
+end
+
+print("Improved Join/Leave System ClientSide By Inj3 - Loaded")
---
+++ improved_joinleave_system/lua/ipr_joinleave_sys/configuration/ipr_configuration.lua
@@ -0,0 +1,41 @@
+--- Script By Inj3 
+--- Script By Inj3 
+--- Script By Inj3 
+--- // https://steamcommunity.com/id/Inj3/
+--- // Improved Join/Leave System
+
+if SERVER then
+     ImprovedJoinLeaveSys.AntiSpam = 10 --- Délai si le joueur tente de se connecter plusieurs fois dans les 10 secondes.
+     ImprovedJoinLeaveSys.BlockName = { --- Bloquer le joueur qui porte un nom qui ne respect pas les règles de votre serveur (Kick).
+         "discord.gg",
+     --"bot",
+     }
+ 
+else
+     ImprovedJoinLeaveSys.HideLeavePlayer = { --- Cacher la notification quand les joueurs quitte le serveur, seuls les groupes ci-dessous peuvent la voir.
+         ["Enable/Disable"] = true, --- Activer = true / Désactiver = false.
+         ["Group"] = { --- Indiquer vos groupes :
+             ["superadmin"] = true,
+         }
+     }
+     ImprovedJoinLeaveSys.JoinLeaveTbl = { --- Phrase lorsque le joueur se connect/termine le chargement/quitte le serveur.
+         ["NameServer"] = "CentralCity", --- Nom de votre Serveur.
+         ["ColorNameServer"] = Color(231, 76, 60), --- Couleur du texte (Nom serveur).
+ 
+         ["ColorPlayerJoin"] = Color(236, 240, 241), --- Couleur Nom du joueur.
+ 
+         [1] = {
+             Texte = "a rejoint le serveur.",
+             Color = Color(231, 76, 60)
+         },
+         [2] = {
+             Texte = "a fini le chargement.",
+             Color = Color(231, 76, 60)
+         },
+         [3] = {
+             Texte = "quitte le serveur.",
+             Color = Color(231, 76, 60)
+         }
+     }
+end
+ 
